6be592225acec4d4985bd82879863649174cb2c0
1XTR.homepc <1XTR.homepc@trendco.space>
2020-06-05 16:16:16 +0300

    HW 1

--- public/index.php
+++ public/index.php
@@ -1,2 +1,22 @@
 <?php
-echo "Hello, world!";
+class Product {
+    public $id;
+    public $name;
+    public $image;
+    public $price;
+
+    public function getAllProducts() {
+    //some code
+    }
+    public function getProductByID($id) {
+        //$sql = "$id";
+    }
+
+}
+
+class CartProduct extends Product {
+    public function __construct($productID) {
+        $cartProduct = self::getProductByID($productID);
+    }
+
+}
